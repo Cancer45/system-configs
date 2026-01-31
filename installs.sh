a357356b7ece23cd38a3cf1c03ee45c986216924
Cancer45 <108489654+Cancer45@users.noreply.github.com>
2026-01-31 16:15:12 +0500

    Create installs.sh

---
+++ installs.sh
@@ -0,0 +1,10 @@
+# xsecurelock neovim zathura pcmanfm brightnessctl inxi feh maim
+
+# xsecurelock -- screen locking utility
+# neovim -- text editor
+# zathura -- doc reader (PDFs)
+# pcmanfm -- GUI file manager
+# brightnessctl -- brightness setting utility
+# inxi -- system information utility
+# feh -- image viewer & wallpaper setting utility
+# maim -- screenshot utility
