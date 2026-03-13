ddccd7397a84bee8f95914d7dac91a2786816b36
Cancer45 <108489654+Cancer45@users.noreply.github.com>
2026-03-13 13:17:19 +0500

    Update installs.sh

--- installs.sh
+++ installs.sh
@@ -1,10 +1,11 @@
-# brightnessctl feh inxi maim neovim pcmanfm picom redshift xbindkeys xclip xss-lock xsecurelock zathura
+# brightnessctl feh inxi maim neovim nerdfonts pcmanfm picom redshift sc-im xbindkeys xclip xss-lock xsecurelock zathura
 
 # brightnessctl -- brightness setting utility
 # feh -- image viewer & wallpaper setting utility
 # inxi -- system information utility
 # maim -- screenshot utility
 # neovim -- text editor
+# nerdfonts -- glyph-patched fonts package
 # pcmanfm -- GUI file manager
 # picom -- compositor
 # redshift -- color temperature utility
@@ -13,4 +14,4 @@
 # xss-lock -- engages screenlocker on suspend; prevents 'peek' vulnerabilities
 # xsecurelock -- screen locking utility
 # zathura -- doc reader (PDFs)
-# nerdfonts -- glyph-patched fonts package
+# sc-im -- ncurses spreadsheet viewer/editor -> xlsx_support[libxml2, libzip, xlsxwriter(lib)]
