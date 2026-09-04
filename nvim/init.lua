f7441b3c1bd17534fe01d65fb34cee167d13d3a4
Cancer45 <108489654+Cancer45@users.noreply.github.com>
2026-09-04 23:12:28 +0500

    Update init.lua

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,16 +1,4 @@
--- ~/.config/nvim
-require("options")
+~/.config/nvim
+require("config.options")
+require("config.lazy")
 require("plugins")
-
--- return cursor to thin on exit
-vim.cmd([[
-    autocmd VimLeave * set guicursor=a:ver100
-]])
-
--- disable nvim bg; fallback to terminal bg
-vim.cmd([[
-    highlight Normal ctermbg=NONE guibg=NONE
-    highlight NormalNC ctermbg=NONE guibg=NONE
-    highlight LineNr ctermbg=NONE guibg=NONE
-    highlight CursorLineNr ctermbg=NONE guibg=NONE
-]])
